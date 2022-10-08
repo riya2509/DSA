f48c4504f9e6533c237e6bcbde0759bb30b0471c
riya2509 <riyamars3@gmail.com>
2022-10-08 15:04:03 +0530

    sum of two numbers

---
+++ FunctionProgram/sum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int sum(int a, int b);
+int main(){
+    int a =4, b= 7;
+    printf("The value after adding 4 and 7 is %d",sum(a,b));
+    return 0;
+}
+int sum(int a, int b){
+    int c;
+    c= a+b;
+    return c;
+}
