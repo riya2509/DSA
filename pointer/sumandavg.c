17016c88d222d2b5d672770d1489e213fa722b67
riya2509 <riyamars3@gmail.com>
2022-10-08 17:51:47 +0530

    sum and avg program corrected

--- pointer/sumandavg.c
+++ pointer/sumandavg.c
@@ -5,7 +5,7 @@ int main(){
     float avg;
     sumandavg(a,b,&sum,&avg);
     printf("The sum is %d",sum);
-    printf("The average is %d", avg);
+    printf("The average is %f", avg);
     return 0;
 }
 void sumandavg(int a , int b,int *sum, float *avg){
