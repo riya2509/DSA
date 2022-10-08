01e83ae1ffc83fd4a7300446a25097e3edbabd43
riya2509 <riyamars3@gmail.com>
2022-10-08 15:30:01 +0530

    sum

--- FunctionProgram/sum.c
+++ FunctionProgram/sum.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 int sum(int a, int b);
-int main(){
-    int a =4, b= 7;
-    printf("The value after adding 4 and 7 is %d",sum(a,b));
+int main()
+{
+    int a = 4, b = 7;
+    printf("The value after adding 4 and 7 is %d", sum(a, b));
     return 0;
 }
-int sum(int a, int b){
+int sum(int a, int b)
+{
     int c;
-    c= a+b;
+    c = a + b;
     return c;
 }
