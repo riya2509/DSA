6e529e2a047c1f1edf398021f0b1ac623a01a0f9
riya2509 <riyamars3@gmail.com>
2022-10-08 13:29:54 +0530

    functions

---
+++ FunctionProgram/dayprinting.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void goodmorning();
+void goodafternoon();
+void goodnight();
+void main(){
+    goodmorning();
+    goodafternoon();
+    goodnight();
+    printf("All the functions are printed\n");
+}
+void goodmorning(){
+    printf("Good Morning\n");
+}
+void goodafternoon(){
+    printf("Good Afternoon\n");
+}
+void goodnight(){
+    printf("Good Night\n");
+}
+
