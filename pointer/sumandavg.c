a0615228002a8c37997c93473833ea7780a35988
riya2509 <riyamars3@gmail.com>
2022-10-08 16:28:56 +0530

    finding sum and average through pointer

---
+++ pointer/sumandavg.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void sumandavg(int a , int b, int *sum, float *avg);
+int main(){
+    int a=3 ,b=4 , sum;
+    float avg;
+    sumandavg(a,b,&sum,&avg);
+    printf("The sum is %d",sum);
+    printf("The average is %d", avg);
+    return 0;
+}
+void sumandavg(int a , int b,int *sum, float *avg){
+    *sum=a+b;
+    *avg=(float)(*sum)/2;
+}
