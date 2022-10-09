c1e1fad609504cfbee2a91c44e34c2c9b14b7ca9
riya2509 <riyamars3@gmail.com>
2022-10-09 18:12:45 +0530

    corrected

--- Array/optimisedinsertion.c
+++ Array/optimisedinsertion.c
@@ -19,12 +19,16 @@ int main()
         scanf("%d",&num);
         printf("Enter the position of the number to be inserted:");
         scanf("%d",&pos);
+        if(pos<=0||pos>n+1)
+        printf("Invalid position");
+        else{
         a[n]=a[pos-1];
         a[pos-1]=num;
         n++;
         printf("The elements are:");
         for(i=0;i<n;i++){
             printf("%d\n",a[i]);
+        }
         }
 
     }
