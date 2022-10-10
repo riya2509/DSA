486aae18ec422f664402cf4df38a2e3a8fd0eb3a
riya2509 <riyamars3@gmail.com>
2022-10-10 18:33:18 +0530

    2D array implementation

---
+++ Array/2Darray.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int a[2][3],i,j;
+    printf("Enter the array elements:");
+    for(i=0;i<2;i++){
+        for(j=0;j<3;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("The array elements are:");
+    for(i=0;i<2;i++){
+        for(j=0;j<3;j++){
+            printf("%d",a[i][j]);
+        }
+    }
+    return 0;
+}
