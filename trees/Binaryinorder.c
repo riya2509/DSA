3db9be819008857a97ddc4ce98968092a762fc08
riya2509 <riyamars3@gmail.com>
2022-11-26 21:17:23 +0530

    Creating binary tree and printing inorder

---
+++ trees/Binaryinorder.c
@@ -0,0 +1,39 @@
+// Binary tree implementation
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node *left, *right;
+};
+struct node *create(){
+    int x;
+    struct node *newnode;
+    newnode=(struct node*)malloc(sizeof(struct node));
+    printf("Enter data(-1 for no node)");
+    scanf("%d",&x);
+    if(x==-1){
+        return 0;
+    }
+    newnode->data=x;
+    printf("Enter the left child of %d",x);
+    newnode->left=create();
+    printf("Enter the right child of %d",x);
+    newnode->right=create();
+    return newnode;
+}
+void traverseInorder(struct node* root)  
+{  
+    if (root == NULL)  
+        return;  
+    traverseInorder(root->left);  
+    printf(" %d ", root->data);  
+    traverseInorder(root->right);  
+}  
+int main(){
+    struct node *root;
+    root=0;
+    root=create();
+    printf("\n The Inorder traversal of given binary tree is -\n");  
+    traverseInorder(root);  
+    return 0;  
+}
